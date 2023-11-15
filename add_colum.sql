7bf04315616b8fc1d32d17926d4fef95f004de15
Piotr Miś <piotrmis84@gmail.com>
2023-11-15 12:59:21 +0100

    add column use_current

---
+++ add_colum.sql
@@ -0,0 +1,37 @@
+Alter table water_social
+Add (
+
+
+    use_current number (5,2)
+
+);
+
+
+Alter table water_prod
+Add (
+
+
+    use_current number (5,2)
+
+);
+
+
+
+Alter table gas_prod
+Add (
+
+
+    use_current number (5,2)
+
+);
+
+
+
+
+Alter table gas_social
+Add (
+
+
+    use_current number (5,2)
+
+);
